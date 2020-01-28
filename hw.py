d3257f645fa59d38ac3f155580b721df29528f63
karynanikitiuk <karynanikitiuk@gmail.com>
2020-01-28 17:23:41 +0700

    add hw update and configfile

--- hw.py
+++ hw.py
@@ -1,16 +1,17 @@
+
 import pytest
 from selenium import webdriver
 import time
 import math
 
 
-@pytest.fixture
-def browser():
-    browser = webdriver.Chrome()
-    browser.implicitly_wait(5)
-
-    yield browser
-    browser.quit()
+#@pytest.fixture
+#def browser():
+#    browser = webdriver.Chrome()
+#    browser.implicitly_wait(10)
+#
+#    yield browser
+#    browser.quit()
 
 @pytest.mark.parametrize('links', ["https://stepik.org/lesson/236895/step/1", "https://stepik.org/lesson/236896/step/1", "https://stepik.org/lesson/236897/step/1", "https://stepik.org/lesson/236898/step/1", "https://stepik.org/lesson/236899/step/1", "https://stepik.org/lesson/236903/step/1", "https://stepik.org/lesson/236904/step/1", "https://stepik.org/lesson/236905/step/1"])
 class TestFeedback(object):
